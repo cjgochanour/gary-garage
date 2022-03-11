e28f9950f3be78491f5a2ed4ed5f25e3c191a6b8
cjgochanour <cjgochanour@gmail.com>
2022-03-11 09:39:28 -0600

    specified properties on new vehicles using object initializers

--- Program.cs
+++ Program.cs
@@ -4,10 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Zero fxs = new Zero();
-            Tesla modelS = new Tesla();
-            Cessna mx410 = new Cessna();
-            Ram bigTruck = new Ram();
+            Zero fxs = new Zero()
+            {
+                MainColor = "Black",
+                MaximumOccupancy = 2,
+                BatteryKWh = 16.4
+            };
+            Tesla modelS = new Tesla()
+            {
+                MainColor = "Silver",
+                MaximumOccupancy = 5,
+                BatteryKWh = 8.3
+            };
+            Cessna mx410 = new Cessna()
+            {
+                MainColor = "White",
+                MaximumOccupancy = 4,
+                FuelCapacity = 1.2
+            };
+            Ram bigTruck = new Ram()
+            {
+                MainColor = "Pink",
+                MaximumOccupancy = 5,
+                FuelCapacity = 1.1
+            };
 
             fxs.Drive();
             modelS.Drive();
